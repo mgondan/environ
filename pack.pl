eb864949bc49849d77723a5539208b21f8ff05b3
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2023-12-29 21:41:18 +0100

    Update pack.pl

--- pack.pl
+++ pack.pl
@@ -1,1 +1,8 @@
-M
+name(environ).
+version('1.0').
+title('Demo package with C code, fetching the program environment').
+keywords([demo, c, environment]).
+author( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
+home('http://www.swi-prolog.org/howto/ForeignPack.html' ).
+download( 'http://www.swi-prolog.org/download/pack/environ-*.tgz' ).
+
