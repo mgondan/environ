a1df9cc9ef3adafcecb4794e427d6aac1a2c9c6c
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-01-02 18:54:54 +0100

    Update environ.c

--- c/environ.c
+++ c/environ.c
@@ -1,12 +1,31 @@
 #include <SWI-Prolog.h>
 #include <string.h>
 
-static functor_t FUNCTOR_equal2;
-#define MAXNAME 512
+typedef struct                  /* define a context structure */
+{ int first;
+} context;
 
 static foreign_t
-pl_environ(term_t list, void* ctx)
-{ extern char **environ;
+pl_environ(term_t list, void* handle)
+{
+  struct context* ctx;
+  switch( PL_foreign_control(handle) )
+  { case PL_FIRST_CALL:
+        if ( !(ctx = malloc(sizeof *ctx)) )
+          return PL_resource_error("memory");
+        first = TRUE ;
+        break;
+    case PL_REDO:
+        ctx = PL_foreign_context_address(handle);
+        break;
+    case PL_PRUNED:
+        ctx = PL_foreign_context_address(handle);
+        // Nothing this time
+        free(ctx);
+        return TRUE;
+  }
+
+  extern char **environ;
   term_t tail = PL_copy_term_ref(list);
   term_t head = PL_new_term_ref();
   char **ep;
@@ -25,16 +44,25 @@ pl_environ(term_t list, void* ctx)
 	   !PL_unify_term(head, PL_FUNCTOR, FUNCTOR_equal2,
 			    PL_MBCHARS, name,
 			    PL_MBCHARS, en+1) )
+	free(ctx) ;
 	return FALSE;
     }
   }
 
-  return PL_unify_nil(tail);
+  if(!PL_unify_nil(tail))
+  { free(ctx) ;
+    return FALSE;
+  }
+
+  // We succeed without a choice point */
+  if (!ctx->first)
+  { free(ctx);
+    return TRUE;
+  }
+
+  first = TRUE ;
+  // We succeed with a choice point */
+  PL_retry_address(ctx);
 }
 
-install_t
-install_environ()
-{ FUNCTOR_equal2 = PL_new_functor(PL_new_atom("="), 2);
-
-  PL_register_foreign("environ", 1, pl_environ, PL_FA_NONDETERMINISTIC);
-}
+static install_t register_foreign("environ", 1, pl_environ, PL_FA_NONDETERMINISTIC);}
