35aed1a38975416bad7e8b6cb3f1169a47646d26
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-01-02 17:48:01 +0100

    Update environ.c

--- c/environ.c
+++ c/environ.c
@@ -5,7 +5,7 @@ static functor_t FUNCTOR_equal2;
 #define MAXNAME 512
 
 static foreign_t
-pl_environ(term_t list)
+pl_environ(term_t list, void* ctx)
 { extern char **environ;
   term_t tail = PL_copy_term_ref(list);
   term_t head = PL_new_term_ref();
@@ -36,5 +36,5 @@ install_t
 install_environ()
 { FUNCTOR_equal2 = PL_new_functor(PL_new_atom("="), 2);
 
-  PL_register_foreign("environ", 1, pl_environ, 0);
+  PL_register_foreign("environ", 1, pl_environ, PL_FA_NONDETERMINISTIC);
 }
